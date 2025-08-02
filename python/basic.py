c312b439bbfb69403dcf85205a6cce65267da34a
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-08-02 15:28:07 +0530

    data science with ml

---
+++ python/basic.py
@@ -0,0 +1,242 @@
+#print("hello hii kese ho ") 
+#1 = "rt"
+#@ =2
+#$ =  2
+#% = 789
+# hello hii
+
+# name = "Ritik Kumar"
+# print(name)
+# na52me = "Ritik Kumar"
+# print(na52me)
+# Age = 22
+# age = 23
+# print(Age)
+# print(age)
+
+
+# name = "Ritik kumar"
+# print("data type of name :-",type(name))
+
+# float 
+# int  
+# str 
+# bool 
+# complex
+# set 
+# dict 
+# tuple
+# # list 
+# name = "hello"
+# print(type(name))
+
+
+
+
+
+# name = "Ankur"
+# print(name)
+# print("type of name :-",type(name))
+# print("len of name :- ",len(name))
+
+# print(name[0])
+# print(name[4])
+
+
+# print(name[0:3]) 
+# print(name[::-1])
+# print(name[-1])
+# lastname = " Saini"
+# print(name+lastname)
+
+# name = "ankur"
+# last  = "Saini"
+# print(name*last) 
+# print(name*3)
+# print(name/last)
+# upper_case=name.upper()
+# print("uper case :-",upper_case)
+# capital = "ANKUR"
+# lower_case =capital.lower()
+# print("lower case :-",lower_case)
+# name.title()
+# name.capitalize()
+# name.casefold()
+# name.count("e")
+
+# name.index()
+# name.find()
+
+# print = 10 
+# print(print)
+
+# list = 10
+
+# print(list)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.list >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+lst = [ 1,2,3,5,8,58,5,8 ,"hello", 0.2] #### syntax
+
+# print(lst)
+# duplicated values allow
+# mutable 
+# orded
+# chang
+# hetroguies or homoguies 
+
+lst = [    1,2,3,5,8,58,5,8 ,0.2] #### syntax
+
+
+# print("This is my first list😍:- ",lst)
+# print("type of lst:-",type(lst))
+# print("len of lst:-",len(lst))
+
+# print(lst[0])
+# print(lst[5])
+# print(lst[0:5])
+# list = [1,2,5,2,58,21,5,24,4,4,1,]
+# lst.append(1000) ##3 last me element add krta hai 
+# print(lst)
+# lst.clear()
+# lst1=lst.copy()
+# lst.count(1)
+# lst.insert(0,500)
+# lst.pop()
+# lst.reverse()
+# # lst.remove()
+# lst.sort()
+# print(lst)
+
+# task1 
+# lst = [1,2,5,2,5,5,[1,5,1,5,2,[8,54,8]]]
+# print(lst[6][5][1])
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>...tuple <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# tpl = (1,2,3,4,5,5,6,7,8,9,"hello") #### syntax
+# print("this is my first tuple:-",tpl)
+# print("type of tpl:-",type(tpl))
+# print("len of tpl:-",len(tpl))
+# print(tpl[0])
+# print(tpl[4])
+# print(tpl[0:6])
+
+# a= 1,2,5
+# print(a)
+# print(type(a))
+# print(len(a))
+
+
+# a, b,c,d,e =(9,1,6,8)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(d)
+# a , b ,c  = [1,2,3]
+# print(a)
+# print(b)
+# print(c)
+# lst =list(tpl)
+# print(lst )
+# print(type(lst))
+# lst.append(1000)
+
+# tpl =tuple(lst)
+# print(tpl)
+
+# tpl = (1,2,3,4,5,5,6,7,8,9) #### syntax
+# # print("govind" in tpl)
+
+# print(max(tpl))
+# print(min(tpl))
+# print(sum(tpl))
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>dict >>>>>>>>>>>>>>>>>>>>>>
+
+# dct = {
+#     "name":"Ritik",
+#     "Class":"2ndyear",
+#     "rollnumber":21
+# }
+# name , class and rollnumber >>>>>keys 
+# Ritik , 2ndyear and value >>>>> values
+# print("this is my first dicdt :-",dct)
+# print("type of dct", type(dct))
+# print("len of dct:-",len(dct))
+# print("dct keys :-",dct.keys())
+# print("dct values :- ",dct.values())
+# print("dct items:- ",dct.items())
+
+# # h.w.
+# # copy  copy kor 
+# # deepcopy 
+# # update 
+# # get 
+
+
+# = == is 
+# dct['address']="jaipur"
+
+
+
+
+
+
+# print(dct)
+# del dct["name"]
+
+# print(dct)
+
+# >>>>>>>>>>>>>>>>>>>>>>set >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# sat = {1,2,32,54,"hello"}
+# print("this is my first set :- ",sat)
+# print("len of sat :- ",len(sat))
+# print("type of sat:-", type(sat))
+
+
+# a = {1,2,3}
+# b = {3,4,5}
+# a.difference(b)
+
+# sat.difference
+# sat.intersection
+# sat.issubset
+# sat.issuperset
+# sat.union
+
+# dict.copy  
+# 
+# 
+dct = {
+    "name":"Ritik",
+    "Class":"2ndyear",
+    "rollnumber":21
+}
+ 
+# import copy 
+# dct1 =copy.deepcopy(dct)
+# print(dct1)
+
+# import copy
+# original = [1, 2, [3, 4]]
+# shallow = copy.copy(original)
+# shallow[2][0] = 99
+# print("Original:", original)
+# print("Shallow:", shallow)
+
+# original.copy()
+# import copy
+# original = [1, 2, [3, 4]]
+# deep = copy.deepcopy(original)
+# deep[2][0] = 99
+# print("Original:", original)
+# print("Deep:", deep)
+# What will be the result of in Python?
+# print(True + True + False) >>>>>>  1 +1 +0
+
+
